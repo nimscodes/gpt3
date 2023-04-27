ddebe92c9eab38a3a724e4fdab78eb033620e6e3
nimscodes <princenimako28@gmail.com>
2023-04-27 13:07:08 +0100

    project set up and navbar and header section done

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+
+import {
+  Footer,
+  Header,
+  Features,
+  Possibility,
+  WhatGPT3,
+  Blog,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import Logo from "../../assets/logo.svg";
+
+import "./navbar.css";
+
+// BEM --> Block Element Modifier
+
+const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href="/">Home</a>
+      </p>
+      <p>
+        <a href="/">What is GPT</a>
+      </p>
+      <p>
+        <a href="/">Open AI</a>
+      </p>
+      <p>
+        <a href="/">Case Studies</a>
+      </p>
+      <p>
+        <a href="/">Library</a>
+      </p>
+    </>
+  );
+};
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign out</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {menu ? (
+          <RiCloseLine color="#fff" size={27} onClick={() => setMenu(false)} />
+        ) : (
+          <RiMenu3Line color="#fff" size={27} onClick={() => setMenu(true)} />
+        )}
+        {menu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign out</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
